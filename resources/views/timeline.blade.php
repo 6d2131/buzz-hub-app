66607265f1ec23df397453ba3cf0b294982ef566
6d2131 <6d2131@gmail.com>
2023-06-24 05:48:42 +0900

    init

---
+++ resources/views/timeline.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></head>
+<body>
+    <div class="container">
+        <div class="row mt-2">
+            <div class="col">
+            </div>
+            <div class="col">
+                <h1 class="text-center">Buzz Hub</h1>
+            </div>
+            <div class="col">
+                <button type="button" class="btn btn-primary my-auto" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                投稿
+                </button>
+            </div>
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mt-2">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <span class="card-title">山田</span>
+                        <span class="card-subtitle mb-2 text-muted">@yamada</span>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <span class="card-subtitle mb-2 text-muted">6時間前</span>
+                    </div>
+                </div>
+                <p class="card-text">こんにちは</p>
+            </div>
+        </div>
+        <div class="card mt-2">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <span class="card-title">山田</span>
+                        <span class="card-subtitle mb-2 text-muted">@yamada</span>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <span class="card-subtitle mb-2 text-muted">6時間前</span>
+                    </div>
+                </div>
+                <p class="card-text">こんにちは</p>
+            </div>
+        </div>
+        <div class="card mt-2">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <span class="card-title">山田</span>
+                        <span class="card-subtitle mb-2 text-muted">@yamada</span>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <span class="card-subtitle mb-2 text-muted">6時間前</span>
+                    </div>
+                </div>
+                <p class="card-text">こんにちは</p>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
